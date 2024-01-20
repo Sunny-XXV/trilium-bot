676a87fbb1f8ec5731e8787271abe3df9369539f
Sun Yan <70095587+Sunny-XXV@users.noreply.github.com>
2024-01-20 22:43:57 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,14 +1,12 @@
 # Trilium bot
-token = "6893169946:AAGtONaLN67QTwC_7hrVLi4E6AN4F-SBBmQ"
+token = ""
 
 # Telegram user id, for example admin_list = [11111,]
 # Only user who is in this list can use this bot
-admin_list = [
-    1260189383,
-]
+admin_list = []
 
 # Trilium ETAPI
 # http_proxy = 'http://127.0.0.1:10809'
 http_proxy = None
-trilium_server_url = "http://localhost:37840"
-etapi_token = "bBXPafPme9O3_2HP2FQL38BUuKNhs8IYVXj8EeGk9qSKoB6ooMNg1mKY="
+trilium_server_url = ""
+etapi_token = ""
